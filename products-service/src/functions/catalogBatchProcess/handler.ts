c5fdeda99bc9a7c5be6acaaf20b8844c971abeb4
yevhen_rud <yevhen_rud@epam.com>
2023-04-10 20:49:51 +0300

    task 7

--- products-service/src/functions/catalogBatchProcess/handler.ts
+++ products-service/src/functions/catalogBatchProcess/handler.ts
@@ -1,5 +1,6 @@
 import {SNS} from 'aws-sdk';
 import { SQSEvent } from 'aws-lambda';
+import { Product } from 'src/models/Products';
 
 const catalogBatchProcess = async (event: SQSEvent) => {
   const products = event.Records.map(({body}) => body)
@@ -13,9 +14,10 @@ const catalogBatchProcess = async (event: SQSEvent) => {
   try {
     for (const product of products) {
 
+      const newProduct = {...product, price: +product.price}
       const response = await sns.publish({
           Subject: 'create product',
-          Message: JSON.stringify(product),
+          Message: product,
           TopicArn: process.env.SNS_TOPIC
         }).promise();
       console.log('response is ->', response);
